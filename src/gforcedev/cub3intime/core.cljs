989e0d9ca957fc4e91e467d2e6ddf3056e7199f4
Gforcedev <tecshotclark@gmail.com>
2021-12-28 16:16:25 +0000

    Initial commit

---
+++ src/gforcedev/cub3intime/core.cljs
@@ -0,0 +1,14 @@
+(ns gforcedev.cub3intime.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+(defn simple-component []
+  [:div
+   [:p "I am a component"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(defn ^:export run []
+  (rdom/render [simple-component] (js/document.getElementById "app")))
+
